ccc965670dfc8e81280af3ca8a153b70cd115aeb
Pulse-Git <47848626+Pulse-Git@users.noreply.github.com>
2021-10-25 11:28:57 +0200

    Basic Implementation

---
+++ KodiPortableStarter/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+
+namespace KodiPortableStarter
+{
+    class Program
+    {
+        public static string folder = "Kodi";
+        public static string app = "kodi.exe";
+
+        static void Main(string[] args)
+        {
+            Process p = new Process();
+            p.StartInfo.FileName = Path.Combine(Path.GetDirectoryName(Assembly.GetEntryAssembly().Location), folder, app);
+            p.StartInfo.Arguments = "-p";
+            p.Start();
+        }
+    }
+}
